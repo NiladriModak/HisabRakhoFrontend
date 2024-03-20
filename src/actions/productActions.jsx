0431daabdde8b47da758e920c73d8e38659d4489
NiladriModak <niladrimodakk2001@gmail.com>
2024-03-20 20:44:47 +0530

    token

--- src/actions/productActions.jsx
+++ src/actions/productActions.jsx
@@ -1,232 +1,329 @@
-import { ADD_FAIL, ADD_REQUEST, ADD_SUCCESS, ALL_CATAGORY_FAIL, ALL_CATAGORY_REQUEST, ALL_CATAGORY_SUCCESS, ALL_PRODUCT_FAIL, ALL_PRODUCT_REQUEST, ALL_PRODUCT_SUCCESS, ALL_VENDOR_FAIL, ALL_VENDOR_REQUEST, ALL_VENDOR_SUCCESS, CLEAR_ERRORS, CREATE_PRODUCT_FAIL, CREATE_PRODUCT_REQUEST, CREATE_PRODUCT_SUCCESS, EDIT_PRODUCT_FAIL, EDIT_PRODUCT_REQUEST, EDIT_PRODUCT_SUCCESS, EMPTY_FAIL, EMPTY_REQUEST, EMPTY_SUCCESS, LARGEST_SOLD_FAIL, LARGEST_SOLD_REQUEST, LARGEST_SOLD_SUCCESS, SINGLE_PRODUCT_FAIL, SINGLE_PRODUCT_REQUEST, SINGLE_PRODUCT_SUCCESS, SINGLE_VENDOR_FAIL, SINGLE_VENDOR_REQUEST, SINGLE_VENDOR_SUCCESS } from "../Constants/productConstant";
-import axios from "../axios_in"
+import {
+  ADD_FAIL,
+  ADD_REQUEST,
+  ADD_SUCCESS,
+  ALL_CATAGORY_FAIL,
+  ALL_CATAGORY_REQUEST,
+  ALL_CATAGORY_SUCCESS,
+  ALL_PRODUCT_FAIL,
+  ALL_PRODUCT_REQUEST,
+  ALL_PRODUCT_SUCCESS,
+  ALL_VENDOR_FAIL,
+  ALL_VENDOR_REQUEST,
+  ALL_VENDOR_SUCCESS,
+  CLEAR_ERRORS,
+  CREATE_PRODUCT_FAIL,
+  CREATE_PRODUCT_REQUEST,
+  CREATE_PRODUCT_SUCCESS,
+  EDIT_PRODUCT_FAIL,
+  EDIT_PRODUCT_REQUEST,
+  EDIT_PRODUCT_SUCCESS,
+  EMPTY_FAIL,
+  EMPTY_REQUEST,
+  EMPTY_SUCCESS,
+  LARGEST_SOLD_FAIL,
+  LARGEST_SOLD_REQUEST,
+  LARGEST_SOLD_SUCCESS,
+  SINGLE_PRODUCT_FAIL,
+  SINGLE_PRODUCT_REQUEST,
+  SINGLE_PRODUCT_SUCCESS,
+  SINGLE_VENDOR_FAIL,
+  SINGLE_VENDOR_REQUEST,
+  SINGLE_VENDOR_SUCCESS,
+} from "../Constants/productConstant";
+import axios from "../axios_in";
 // import axios from "axios"
-export const getProduct=(keyword = "",category)=>async(dispatch)=>{
-    try{
-        const port="http://localhost:80";
-        dispatch({type: ALL_PRODUCT_REQUEST});
-        const config={
-            headers:{
-                "Content-Type": "application/json",
-                // "Cookie": `token=${localStorage.getItem("UserToken")}`
-            },
-            withCredentials: true,
-        }
-        let link = `/api/allProducts?keyword=${keyword}`;
-
-        if (category) {
-            link = `/api/allProducts?keyword=${keyword}&&catagory=${category}`;
-        }
-        const {data} = await axios.get(link,config);
-        // console.log("The all product user action  - ",data)
-        dispatch({type:ALL_PRODUCT_SUCCESS,payload:data});
-    }catch(error){
-        dispatch({type:ALL_PRODUCT_FAIL,payload:error.response.data.message,})
-    }
-}
-
-
-
-
-export const getSingleProductDetails=(id)=>async(dispatch)=>{
+export const getProduct =
+  (keyword = "", category) =>
+  async (dispatch) => {
+    const usertoken = localStorage.getItem("UserToken");
     try {
-        const port="http://localhost:80";
-        dispatch({type: SINGLE_PRODUCT_REQUEST});
-        const config={
-            headers:{"Content-Type": "application/json"},
-            withCredentials: true,
-        }
-        const {data} = await axios.get(`/api/product/${id}`,config)
-        // console.log("single",data)
-        dispatch({type:SINGLE_PRODUCT_SUCCESS,payload:data});
-    } catch (error) {
-        dispatch({type:SINGLE_PRODUCT_FAIL,payload:error.response.data.message,})
-    }
-}
-
-export const AddStock=(GivenData,id)=>async(dispatch)=>{
-    try {
-        const port="http://localhost:80";
-        dispatch({type: ADD_REQUEST});
-
-        const config={
-            headers:{"Content-Type": "application/json"},
-            withCredentials: true,
-        }
-        const {data} = await axios.put(`/api/product/${id}/AddStock`,GivenData,config)
-
-        dispatch({type:ADD_SUCCESS,payload:data});
-    } catch (error) {
-        dispatch({type:ADD_FAIL,payload:error.response.data.message,})
-    }
-}
-
-
-export const EmptyStock=(GivenData,id)=>async(dispatch)=>{
-    try {
-        const port="http://localhost:80";
-        dispatch({type: EMPTY_REQUEST});
-
-        const config={
-            headers:{"Content-Type": "application/json"},
-            withCredentials: true,
-        }
-        const {data} = await axios.put(`/api/product/${id}/EmptyStock`,GivenData,config)
-
-        dispatch({type:EMPTY_SUCCESS,payload:data});
-    } catch (error) {
-        dispatch({type:EMPTY_FAIL,payload:error.response.data.message,})
-    }
-}
-
-export const AllCatagory=(keyword="")=>async(dispatch)=>{
-    try {
-        const port="http://localhost:80";
-        dispatch({type: ALL_CATAGORY_REQUEST});
-
-        const config={
-            headers:{"Content-Type": "application/json",
-                // 'Cookie': `token=${localStorage.getItem("UserToken")}`
-            },
-            withCredentials: true,
-        }
-
-        const {data} = await axios.get(`/api/allCatagory`,config)
-        
-        let tobesend=data.p;
-
-        const filteredCategories = tobesend.filter(category =>
-            category.toLowerCase().includes(keyword.toLowerCase())
-          );
-
-        dispatch({type:ALL_CATAGORY_SUCCESS,payload:filteredCategories});
-    } catch (error) {
-        dispatch({type:ALL_CATAGORY_FAIL,payload:error.response.data.message,})
-    }
-}
-
-export const getLargestSoldProduct=()=>async(dispatch)=>{
-    try {
-        const port="http://localhost:80";
-        dispatch({type: LARGEST_SOLD_REQUEST});
-        const config={
-            headers:{"Content-Type": "application/json"},
-            withCredentials: true,
-        }
-        const {data} = await axios.get(`/api/largestSalingProduct`,config)
-
-        dispatch({type:LARGEST_SOLD_SUCCESS,payload:data})
-    } catch (error) {
-        dispatch({type:LARGEST_SOLD_FAIL,payload:error.response.data.message,})
-    }
-        
-}
-
-export const AllVendor=(vendorName)=>async(dispatch)=>{
-    try {
-        const port="http://localhost:80";
-        dispatch({type: ALL_VENDOR_REQUEST});
-
-        const config={
-            headers:{"Content-Type": "application/json",
-            // 'Cookie': `token=${localStorage.getItem("UserToken")}`
+      const port = "http://localhost:80";
+      dispatch({ type: ALL_PRODUCT_REQUEST });
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          // "Cookie": `token=${localStorage.getItem("UserToken")}`
+          Authorization: `Bearer ${usertoken}`,
         },
-            withCredentials: true,
-        }
+        withCredentials: true,
+      };
+      let link = `/api/allProducts?keyword=${keyword}`;
 
-        let link = `/api/allVendor`;
-        if (vendorName) {
-            link = `/api/allVendor?vendorName=${vendorName}`;
-
-        }
-        const {data} = await axios.get(link,config);
-
-        dispatch({type:ALL_VENDOR_SUCCESS,payload:data});
+      if (category) {
+        link = `/api/allProducts?keyword=${keyword}&&catagory=${category}`;
+      }
+      const { data } = await axios.get(link, config);
+      // console.log("The all product user action  - ",data)
+      dispatch({ type: ALL_PRODUCT_SUCCESS, payload: data });
     } catch (error) {
-        dispatch({type:ALL_VENDOR_FAIL,payload:error.response.data.message,})
+      dispatch({
+        type: ALL_PRODUCT_FAIL,
+        payload: error.response.data.message,
+      });
     }
-}
+  };
 
-export const getSingleVendor=(id)=>async(dispatch)=>{
+export const getSingleProductDetails = (id) => async (dispatch) => {
+  try {
+    const port = "http://localhost:80";
+    dispatch({ type: SINGLE_PRODUCT_REQUEST });
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${usertoken}`,
+      },
+      withCredentials: true,
+    };
+    const { data } = await axios.get(`/api/product/${id}`, config);
+    // console.log("single",data)
+    dispatch({ type: SINGLE_PRODUCT_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: SINGLE_PRODUCT_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+
+export const AddStock = (GivenData, id) => async (dispatch) => {
+  try {
+    const port = "http://localhost:80";
+    dispatch({ type: ADD_REQUEST });
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${usertoken}`,
+      },
+      withCredentials: true,
+    };
+    const { data } = await axios.put(
+      `/api/product/${id}/AddStock`,
+      GivenData,
+      config
+    );
+
+    dispatch({ type: ADD_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({ type: ADD_FAIL, payload: error.response.data.message });
+  }
+};
+
+export const EmptyStock = (GivenData, id) => async (dispatch) => {
+  try {
+    const port = "http://localhost:80";
+    dispatch({ type: EMPTY_REQUEST });
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${usertoken}`,
+      },
+      withCredentials: true,
+    };
+    const { data } = await axios.put(
+      `/api/product/${id}/EmptyStock`,
+      GivenData,
+      config
+    );
+
+    dispatch({ type: EMPTY_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({ type: EMPTY_FAIL, payload: error.response.data.message });
+  }
+};
+
+export const AllCatagory =
+  (keyword = "") =>
+  async (dispatch) => {
     try {
-        const port="http://localhost:80";
-        dispatch({type:SINGLE_VENDOR_REQUEST})
-        const config={
-            headers:{"Content-Type": "application/json"},
-            withCredentials: true,
-        }
-        const {data} = await axios.get(`/api/allVendor/${id}`,config)
-        dispatch({type:SINGLE_VENDOR_SUCCESS,payload:data.vendor})
+      const port = "http://localhost:80";
+      dispatch({ type: ALL_CATAGORY_REQUEST });
+
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          // 'Cookie': `token=${localStorage.getItem("UserToken")}`
+          Authorization: `Bearer ${usertoken}`,
+        },
+        withCredentials: true,
+      };
+
+      const { data } = await axios.get(`/api/allCatagory`, config);
+
+      let tobesend = data.p;
+
+      const filteredCategories = tobesend.filter((category) =>
+        category.toLowerCase().includes(keyword.toLowerCase())
+      );
+
+      dispatch({ type: ALL_CATAGORY_SUCCESS, payload: filteredCategories });
     } catch (error) {
-        dispatch({type:SINGLE_VENDOR_FAIL,payload:error.response.data.message})
+      dispatch({
+        type: ALL_CATAGORY_FAIL,
+        payload: error.response.data.message,
+      });
     }
-}
+  };
 
-export const UpdatePayments=(id,GivenData)=>async(dispatch)=>{
-    try {
-        const port="http://localhost:80";
-        dispatch({type:SINGLE_VENDOR_REQUEST})
-        const config={
-            headers:{"Content-Type": "application/json"},
-            withCredentials: true,
-        }
-        const {data} = await axios.put(`/api/allVendor/${id}/updatePayment`,GivenData,config)
-        dispatch({type:SINGLE_VENDOR_SUCCESS,payload:data.vendor})
-    } catch (error) {
-        dispatch({type:SINGLE_VENDOR_FAIL,payload:error.response.data.message})
+export const getLargestSoldProduct = () => async (dispatch) => {
+  try {
+    const port = "http://localhost:80";
+    dispatch({ type: LARGEST_SOLD_REQUEST });
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${usertoken}`,
+      },
+      withCredentials: true,
+    };
+    const { data } = await axios.get(`/api/largestSalingProduct`, config);
+
+    dispatch({ type: LARGEST_SOLD_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({ type: LARGEST_SOLD_FAIL, payload: error.response.data.message });
+  }
+};
+
+export const AllVendor = (vendorName) => async (dispatch) => {
+  try {
+    const port = "http://localhost:80";
+    dispatch({ type: ALL_VENDOR_REQUEST });
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${usertoken}`,
+        // 'Cookie': `token=${localStorage.getItem("UserToken")}`
+      },
+      withCredentials: true,
+    };
+
+    let link = `/api/allVendor`;
+    if (vendorName) {
+      link = `/api/allVendor?vendorName=${vendorName}`;
     }
-}
+    const { data } = await axios.get(link, config);
 
-export const PrevPayments=(id,GivenData)=>async(dispatch)=>{
-    try {
-        const port="http://localhost:80";
-        dispatch({type:SINGLE_VENDOR_REQUEST})
-        const config={
-            headers:{"Content-Type": "application/json"},
-            withCredentials: true,
-        }
-        const {data} = await axios.put(`/api/allVendor/${id}/prevPayment`,GivenData,config)
-        dispatch({type:SINGLE_VENDOR_SUCCESS,payload:data.vendor})
-    } catch (error) {
-        dispatch({type:SINGLE_VENDOR_FAIL,payload:error.response.data.message})
-    }
-}
+    dispatch({ type: ALL_VENDOR_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({ type: ALL_VENDOR_FAIL, payload: error.response.data.message });
+  }
+};
 
-export const createProduct=(GivenData)=>async(dispatch)=>{
-    try {
-        const port="http://localhost:80";
-        dispatch({type:CREATE_PRODUCT_REQUEST});
-        const config={
-            headers:{"Content-Type": "application/json"},
-            withCredentials: true,
-        }
-        const {data} = await axios.post(`/api/create`,GivenData,config)
+export const getSingleVendor = (id) => async (dispatch) => {
+  try {
+    const port = "http://localhost:80";
+    dispatch({ type: SINGLE_VENDOR_REQUEST });
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${usertoken}`,
+      },
+      withCredentials: true,
+    };
+    const { data } = await axios.get(`/api/allVendor/${id}`, config);
+    dispatch({ type: SINGLE_VENDOR_SUCCESS, payload: data.vendor });
+  } catch (error) {
+    dispatch({
+      type: SINGLE_VENDOR_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
 
-        dispatch({type:CREATE_PRODUCT_SUCCESS,payload:data.product}) 
-    } catch (error) {
-        dispatch({type:CREATE_PRODUCT_FAIL,payload:error.response.data.message})
-    }
+export const UpdatePayments = (id, GivenData) => async (dispatch) => {
+  try {
+    const port = "http://localhost:80";
+    dispatch({ type: SINGLE_VENDOR_REQUEST });
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${usertoken}`,
+      },
+      withCredentials: true,
+    };
+    const { data } = await axios.put(
+      `/api/allVendor/${id}/updatePayment`,
+      GivenData,
+      config
+    );
+    dispatch({ type: SINGLE_VENDOR_SUCCESS, payload: data.vendor });
+  } catch (error) {
+    dispatch({
+      type: SINGLE_VENDOR_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
 
-}
+export const PrevPayments = (id, GivenData) => async (dispatch) => {
+  try {
+    const port = "http://localhost:80";
+    dispatch({ type: SINGLE_VENDOR_REQUEST });
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${usertoken}`,
+      },
+      withCredentials: true,
+    };
+    const { data } = await axios.put(
+      `/api/allVendor/${id}/prevPayment`,
+      GivenData,
+      config
+    );
+    dispatch({ type: SINGLE_VENDOR_SUCCESS, payload: data.vendor });
+  } catch (error) {
+    dispatch({
+      type: SINGLE_VENDOR_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
 
+export const createProduct = (GivenData) => async (dispatch) => {
+  try {
+    const port = "http://localhost:80";
+    dispatch({ type: CREATE_PRODUCT_REQUEST });
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${usertoken}`,
+      },
+      withCredentials: true,
+    };
+    const { data } = await axios.post(`/api/create`, GivenData, config);
 
-export const editProduct=(GivenData,id)=>async(dispatch)=>{
-    try {
-        const port="http://localhost:80";
-        dispatch({type:EDIT_PRODUCT_REQUEST});
-        const config={
-            headers:{"Content-Type": "application/json"},
-            withCredentials: true,
-        }
-        const {data} = await axios.put(`/api/product/${id}`,GivenData,config)
+    dispatch({ type: CREATE_PRODUCT_SUCCESS, payload: data.product });
+  } catch (error) {
+    dispatch({
+      type: CREATE_PRODUCT_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
 
-        dispatch({type:EDIT_PRODUCT_SUCCESS,payload:data.product}) 
-    } catch (error) {
-        dispatch({type:EDIT_PRODUCT_FAIL,payload:error.response.data.message})
-    }
+export const editProduct = (GivenData, id) => async (dispatch) => {
+  try {
+    const port = "http://localhost:80";
+    dispatch({ type: EDIT_PRODUCT_REQUEST });
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${usertoken}`,
+      },
+      withCredentials: true,
+    };
+    const { data } = await axios.put(`/api/product/${id}`, GivenData, config);
 
-}
+    dispatch({ type: EDIT_PRODUCT_SUCCESS, payload: data.product });
+  } catch (error) {
+    dispatch({ type: EDIT_PRODUCT_FAIL, payload: error.response.data.message });
+  }
+};
 
-export const clearError=()=>async(dispatch)=>{
-    dispatch({type:CLEAR_ERRORS})
-}
+export const clearError = () => async (dispatch) => {
+  dispatch({ type: CLEAR_ERRORS });
+};
--- src/actions/userAction.jsx
+++ src/actions/userAction.jsx
@@ -1,105 +1,133 @@
-import axios from "../axios_in"
+import axios from "../axios_in";
 // import axios from "axios";
-import { FORGET_PASSWORD_FAIL, FORGET_PASSWORD_REQUEST, FORGET_PASSWORD_SUCCESS, LOAD_USER_FAIL, LOAD_USER_REQUEST, LOAD_USER_SUCCESS, LOGIN_USER_FAIL, LOGIN_USER_REQUEST, LOGIN_USER_SUCCESS, LOGOUT_USER_FAIL, LOGOUT_USER_SUCCESS, REGISTER_USER_FAIL, REGISTER_USER_REQUEST, REGISTER_USER_SUCCESS, RESET_PASSWORD_FAIL, RESET_PASSWORD_REQUEST, RESET_PASSWORD_SUCCESS } from "../Constants/userConstants"
+import {
+  FORGET_PASSWORD_FAIL,
+  FORGET_PASSWORD_REQUEST,
+  FORGET_PASSWORD_SUCCESS,
+  LOAD_USER_FAIL,
+  LOAD_USER_REQUEST,
+  LOAD_USER_SUCCESS,
+  LOGIN_USER_FAIL,
+  LOGIN_USER_REQUEST,
+  LOGIN_USER_SUCCESS,
+  LOGOUT_USER_FAIL,
+  LOGOUT_USER_SUCCESS,
+  REGISTER_USER_FAIL,
+  REGISTER_USER_REQUEST,
+  REGISTER_USER_SUCCESS,
+  RESET_PASSWORD_FAIL,
+  RESET_PASSWORD_REQUEST,
+  RESET_PASSWORD_SUCCESS,
+} from "../Constants/userConstants";
 import { CLEAR_ERRORS } from "../Constants/productConstant";
 import toast from "react-hot-toast";
-export const userLogin=(GivenData)=>async(dispatch)=>{
-    try {
-        dispatch({type:LOGIN_USER_REQUEST})
-        // const port="http://localhost:80";
-        const config={
-            headers:{"Content-Type": "application/json"},
-            withCredentials: true,
-        }
-        const {data} = await axios.post(`/api/login`,GivenData,config);
-        // console.log(data)
-        localStorage.setItem("UserToken",data.token)
-        localStorage.setItem("UserName",data.user.name)
-        dispatch({type:LOGIN_USER_SUCCESS,payload:data})
-    } catch (error) {
-        dispatch({ type: LOGIN_USER_FAIL, payload: error.response.data.message });
-    }
-}
-export const loadUser=()=>async(dispatch)=>{
-    try {
-        // const port="http://localhost:80";
-        dispatch({type:LOAD_USER_REQUEST})
-        const {data} = await axios.get(`/api/me`);
-        dispatch({type:LOAD_USER_SUCCESS,payload:data})
-    } catch (error) {
-        dispatch({type:LOAD_USER_FAIL,
-        payload:error.response.data.message})
-    }
-}
+export const userLogin = (GivenData) => async (dispatch) => {
+  try {
+    dispatch({ type: LOGIN_USER_REQUEST });
+    // const port="http://localhost:80";
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      withCredentials: true,
+    };
+    const { data } = await axios.post(`/api/login`, GivenData, config);
+    // console.log(data)
+    localStorage.setItem("UserToken", data.token);
+    localStorage.setItem("UserName", data.user.name);
+    dispatch({ type: LOGIN_USER_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({ type: LOGIN_USER_FAIL, payload: error.response.data.message });
+  }
+};
+export const loadUser = () => async (dispatch) => {
+  try {
+    // const port="http://localhost:80";
+    dispatch({ type: LOAD_USER_REQUEST });
+    const { data } = await axios.get(`/api/me`);
+    dispatch({ type: LOAD_USER_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({ type: LOAD_USER_FAIL, payload: error.response.data.message });
+  }
+};
 
-export const logout=()=>async(dispatch)=>{
-    try {
-        // const port="http://localhost:80";
-        const {data}=await axios.post(`/api/logout`);
-        // console.log(data)
-        localStorage.removeItem("UserToken")
-        localStorage.removeItem("UserName")
-        dispatch({type:LOGOUT_USER_SUCCESS})
-    } catch (error) {
-        dispatch({type:LOGOUT_USER_FAIL,
-        payload:error.response.data.message})
-    }
-}
+export const logout = () => async (dispatch) => {
+  try {
+    // const port="http://localhost:80";
+    const { data } = await axios.post(`/api/logout`);
+    // console.log(data)
+    localStorage.removeItem("UserToken");
+    localStorage.removeItem("UserName");
+    dispatch({ type: LOGOUT_USER_SUCCESS });
+  } catch (error) {
+    dispatch({ type: LOGOUT_USER_FAIL, payload: error.response.data.message });
+  }
+};
 
 // Register
 export const register = (userData) => async (dispatch) => {
-    try {
-      dispatch({ type: REGISTER_USER_REQUEST });
-  
-      const config={
-        headers:{"Content-Type": "application/json"},
-        withCredentials: true,
-    }
-  
+  try {
+    dispatch({ type: REGISTER_USER_REQUEST });
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      withCredentials: true,
+    };
+
     const { data } = await axios.post(`/api/register`, userData, config);
     // console.log(data)
-    localStorage.setItem("UserToken",data.token)
-    localStorage.setItem("UserName",data.user.name)
-      dispatch({ type: REGISTER_USER_SUCCESS, payload: data.user });
-    } catch (error) {
-      dispatch({
-        type: REGISTER_USER_FAIL,
-        payload: error.response.data.message,
-      });
-    }
-  };
+    localStorage.setItem("UserToken", data.token);
+    localStorage.setItem("UserName", data.user.name);
+    dispatch({ type: REGISTER_USER_SUCCESS, payload: data.user });
+  } catch (error) {
+    dispatch({
+      type: REGISTER_USER_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
 
-
-  //forget password
-  export const forgetPassword = (email) => async (dispatch) => {
-    try {
-      dispatch({ type: FORGET_PASSWORD_REQUEST });
-      const config={
-        headers:{"Content-Type": "application/json"},
-        withCredentials: true,
-    }
+//forget password
+export const forgetPassword = (email) => async (dispatch) => {
+  try {
+    dispatch({ type: FORGET_PASSWORD_REQUEST });
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      withCredentials: true,
+    };
     // console.log("elo")
-    const { data } = await axios.post(`/api/password/forgot`,{email},config);
+    const { data } = await axios.post(
+      `/api/password/forgot`,
+      { email },
+      config
+    );
     // console.log(data)
 
-
-      dispatch({ type: FORGET_PASSWORD_SUCCESS, payload: data });
-      toast.success(data.message)
-    } catch (error) {
-      dispatch({
-        type: FORGET_PASSWORD_FAIL,
-        payload: error.response.data.message,
-      });
-      toast.error(error.response.data.message)
-    }
-  };
+    dispatch({ type: FORGET_PASSWORD_SUCCESS, payload: data });
+    toast.success(data.message);
+  } catch (error) {
+    dispatch({
+      type: FORGET_PASSWORD_FAIL,
+      payload: error.response.data.message,
+    });
+    toast.error(error.response.data.message);
+  }
+};
 
 //reset password
 export const resetPassword = (token, passwords) => async (dispatch) => {
   try {
     dispatch({ type: RESET_PASSWORD_REQUEST });
 
-    const config = { headers: { "Content-Type": "application/json" } };
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
 
     const { data } = await axios.put(
       `/api/password/reset/${token}`,
@@ -116,7 +144,6 @@ export const resetPassword = (token, passwords) => async (dispatch) => {
   }
 };
 
-
-export const clearError=()=>async(dispatch)=>{
-    dispatch({type:CLEAR_ERRORS})
-}
+export const clearError = () => async (dispatch) => {
+  dispatch({ type: CLEAR_ERRORS });
+};
